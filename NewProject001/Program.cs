2728b3a5c4c59a30451a2adc2dceca7f2bcc7459
siyeon13 <siyeon0713@gmail.com>
2023-05-02 14:09:29 +0900

    new project commit

---
+++ NewProject001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string age = Console.ReadLine();
+
+        }
+    }
+}
