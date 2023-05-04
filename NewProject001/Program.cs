2130f37593d24794bcb442eb0296792c313057f4
siyeon13 <siyeon0713@gmail.com>
2023-05-04 16:36:09 +0900

    박준 3052 문제 풀이 Commit

--- NewProject001/Program.cs
+++ NewProject001/Program.cs
@@ -10,14 +10,31 @@ namespace NewProject001
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            string age = Console.ReadLine();
 
-            string add = Console.ReadLine();
-            string id = Console.ReadLine();
-            string pw = Console.ReadLine();
+            int maxCnt = 10;
+            int divNum = 42;
+            int remainder = 0;
+            bool[] isRemain = new bool[divNum];
 
-            Console.WriteLine(name + " / " + age + " / " + add + " / " + id + " / " + pw);
+            for(int i=0; i< maxCnt; i++)
+            {
+                remainder = int.Parse(Console.ReadLine()) % divNum;
+                isRemain[remainder] = true;
+            }
+
+            int cnt = 0;
+            for(int i =0; i < isRemain.Length; i++)
+            {
+                if(isRemain[i] == true)
+                {
+                    cnt++;
+                }
+            }
+           
+            Console.WriteLine("답 : " + cnt);
+
+
+
 
         }
     }
